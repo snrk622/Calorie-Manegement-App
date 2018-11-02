2033ddc7ce53776218ad3a144a393bc76ab82cfe
snrk622 <s.0622.r@icloud.com>
2018-11-02 15:23:04 +0900

    基本的な機能を実装

--- calorieManegeApp/CalorieViewController.swift
+++ calorieManegeApp/CalorieViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class CalorieViewController: UIViewController {
 
-    var getCalorie: String?
+    var getCalorie: UInt16?
+    
+    let userDefaults = UserDefaults.standard
     
     @IBOutlet weak var calorieTextField: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //saveButtonを無効にする
+        self.saveButton.isEnabled = false
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        //このモーダルを閉じる
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func calorieTextFieldChanged(_ sender: Any) {
+        //calorieにcalorieTextFieldのtextを代入
+        let calorie = self.calorieTextField.text ?? ""
+        //calorieが空じゃなかったらsaveButtonを有効にする
+        self.saveButton.isEnabled = !calorie.isEmpty
     }
     
     //segueが巻き戻るときの処理
@@ -27,8 +44,14 @@ class CalorieViewController: UIViewController {
             return
         }
         
-        getCalorie = calorieTextField.text
+        //getCalorieにcalorieTextFieldのtextを代入
+        getCalorie = UInt16(calorieTextField.text!)
         
+    }
+    
+    //タップしたらキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }
--- calorieManegeApp/ViewController.swift
+++ calorieManegeApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //userDfaultsのインスタンスを取得
+    let userDefaults = UserDefaults.standard
+    var sum: UInt16 = 0
+    
     @IBOutlet weak var calorieLabel: UILabel!
     
     //segueを巻き戻したときの処理
@@ -17,15 +21,31 @@ class ViewController: UIViewController {
         guard let sourceVC = sender.source as? CalorieViewController, let getCalorie = sourceVC.getCalorie else {
             return
         }
+        //userDefaultsをsumに読み込み
+        sum = UInt16(userDefaults.integer(forKey: "calorie_value"))
+        //sumにgetcalorieを足す
+        sum = sum + getCalorie
+        //userDefaultsに新しいsumを保存
+        userDefaults.set(sum, forKey: "calorie_value")
+        //calorieLabelにuserDefaultsを表示
+        calorieLabel.text = userDefaults.string(forKey: "calorie_value")
         
-        self.calorieLabel.text = getCalorie
     }
     
-    //userDfaultsのインスタンスを取得
-    let userDefaults = UserDefaults.standard
+    @IBAction func resetButtonAction(_ sender: Any) {
+        //sumをリセット
+        sum = 0
+        //userDefaultsに新しいsumを保存
+        userDefaults.set(sum, forKey: "calorie_value")
+        //calorieLabelにuserDefaultsを表示
+        calorieLabel.text = userDefaults.string(forKey: "calorie_value")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //calorieLabelにuserDefaultsを表示
+        calorieLabel.text = userDefaults.string(forKey: "calorie_value")
         
     }
     
