8b3cc6922b0de8f4649b048191a9f40ab581a6e6
snrk622 <s.0622.r@icloud.com>
2018-11-02 15:50:33 +0900

    リセットの確認ダイアログを追加

--- calorieManegeApp/ViewController.swift
+++ calorieManegeApp/ViewController.swift
@@ -32,13 +32,43 @@ class ViewController: UIViewController {
         
     }
     
+    //resetButtonが押されたら実行
     @IBAction func resetButtonAction(_ sender: Any) {
-        //sumをリセット
-        sum = 0
-        //userDefaultsに新しいsumを保存
-        userDefaults.set(sum, forKey: "calorie_value")
-        //calorieLabelにuserDefaultsを表示
-        calorieLabel.text = userDefaults.string(forKey: "calorie_value")
+        
+        // ① UIAlertControllerクラスのインスタンスを生成
+        // タイトル, メッセージ, Alertのスタイルを指定する
+        // 第3引数のpreferredStyleでアラートの表示スタイルを指定する
+        let alert: UIAlertController = UIAlertController(title: "今日のカロリーをリセットします", message: "本当によろしいですか？", preferredStyle:  UIAlertController.Style.actionSheet)
+        
+        // ② Actionの設定
+        // Action初期化時にタイトル, スタイル, 押された時に実行されるハンドラを指定する
+        // 第3引数のUIAlertActionStyleでボタンのスタイルを指定する
+        // OKボタン
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
+            // ボタンが押された時の処理を書く（クロージャ実装）
+            (action: UIAlertAction!) -> Void in
+            //sumをリセット
+            self.sum = 0
+            //userDefaultsに新しいsumを保存
+            self.userDefaults.set(self.sum, forKey: "calorie_value")
+            //calorieLabelにuserDefaultsを表示
+            self.calorieLabel.text = self.userDefaults.string(forKey: "calorie_value")
+        })
+        
+        // キャンセルボタン
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler:{
+            // ボタンが押された時の処理を書く（クロージャ実装）
+            (action: UIAlertAction!) -> Void in
+            return
+        })
+        
+        // ③ UIAlertControllerにActionを追加
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        
+        // ④ Alertを表示
+        present(alert, animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
